020410bd32bbb9b19ce21743ca0e39b95f41becf
Alexis BUSSENEAU <busseneau.e1302265@etud.univ-ubs.fr>
2015-02-05 11:40:10 +0100

    New version

--- app/src/main/java/com/alexis/done/view/activities/MainActivity.java
+++ app/src/main/java/com/alexis/done/view/activities/MainActivity.java
@@ -16,9 +16,6 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Intent tmp = new Intent(this, AddTaskActivity.class);
-        startActivityForResult(tmp, 1);
     }
 
 
@@ -31,15 +28,8 @@ public class MainActivity extends ActionBarActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        Intent addTask = new Intent(this, AddTaskActivity.class);
+        startActivityForResult(addTask, 1);
 
         return super.onOptionsItemSelected(item);
     }
